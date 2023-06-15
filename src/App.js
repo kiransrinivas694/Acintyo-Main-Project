445cff470bf8ad87b714afab0faee8008846250a
kiran <kiransrinivasnandamuri@gmail.com>
2023-06-15 14:33:13 +0530

    Header Created And Routes Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header";
+import Header from "./components/pages/Header";
+import Home from "./components/pages/Home";
+import AboutUs from "./components/pages/AboutUs";
+import Careers from "./components/pages/Careers";
+import Portfolio from "./components/pages/Portfolio";
+import Contact from "./components/pages/Contact";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <h1>App.js File</h1>
+      <Header />
+      <Routes>
+        <Route exact path="/home" element={<Home />} />
+        <Route exact path="/aboutus" element={<AboutUs />} />
+        <Route exact path="/careers" element={<Careers />} />
+        <Route exact path="/portfolio" element={<Portfolio />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route path="/" element={<Navigate replace to="/home" />} />
+      </Routes>
     </div>
   );
 }
