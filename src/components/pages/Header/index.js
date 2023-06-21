d31a706680c402715a48d0d736f01e28c6330feb
kiran <kiransrinivasnandamuri@gmail.com>
2023-06-21 15:10:37 +0530

    Revert "Revert "Revert "Merge pull request #4 from kiransrinivas694/ticket-1"""
    This reverts commit cdb049c665519cb5eabc040dfcb50b53291401fc.

--- src/components/pages/Header/index.js
+++ src/components/pages/Header/index.js
@@ -1,51 +1,24 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { BsFillTelephoneFill } from "react-icons/bs";
 import { FiMail } from "react-icons/fi";
-import { BiArrowFromTop, BiArrowToTop } from "react-icons/bi";
-import { GiHamburgerMenu } from "react-icons/gi";
 import "./index.css";
 
 function Header() {
   const location = window.location.pathname.slice(1);
 
   const [activeTab, setActiveTab] = useState(location);
-  const [isTopHeaderShowing, setIsTopHeaderShowing] = useState(false);
-  const [isSmallNavShowing, setIsSmallNavShowing] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth > 768) {
-        setIsSmallNavShowing(false);
-        setIsTopHeaderShowing(false);
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
 
   const handleLinkClick = (item) => {
     setActiveTab(item.path);
   };
 
-  const handleTopHeaderChangeClick = () => {
-    setIsTopHeaderShowing(!isTopHeaderShowing);
-  };
-
-  const handleSmallNavBarClick = () => {
-    setIsSmallNavShowing(!isSmallNavShowing);
-  };
-
   const navItems = [
-    { path: "home", navItem: "HOME" },
-    { path: "aboutus", navItem: "ABOUT US" },
-    { path: "portfolio", navItem: "PORTFOLIO" },
-    { path: "careers", navItem: "CAREERS" },
-    { path: "contact", navItem: "CONTACT" },
+    { path: "home", navItem: "Home" },
+    { path: "aboutus", navItem: "About Us" },
+    { path: "portfolio", navItem: "Portfolio" },
+    { path: "careers", navItem: "Careers" },
+    { path: "contact", navItem: "Contact" },
   ];
 
   const content = navItems.map((navItem) => {
@@ -67,25 +40,6 @@ function Header() {
     );
   });
 
-  const smallNavContent = navItems.map((navItem) => {
-    return (
-      <li className="small-nav-list-item">
-        <Link to={`/${navItem.path}`} className="small-nav-item-link">
-          {navItem.navItem}
-        </Link>
-      </li>
-    );
-  });
-
-  const pageHeading = navItems.filter((navItem) => navItem.path === activeTab);
-
-  let isHome;
-  if (pageHeading.length === 0) {
-    isHome = true;
-  } else {
-    isHome = pageHeading[0].path === "home";
-  }
-
   return (
     <div className="header">
       <div className="top-header-section">
@@ -102,57 +56,13 @@ function Header() {
           </div>
         </div>
       </div>
-      <div className="top-header-section-small">
-        <div
-          className={`top-header-main-small ${
-            !isTopHeaderShowing && "is-top-header-showing"
-          }`}
-        >
-          <div className="top-header-item">
-            <BsFillTelephoneFill className="header-icons" />
-            <p>Phone : +91 96525 97911</p>
-          </div>
-          <div className="top-header-item">
-            <FiMail className="header-icons" />
-            <p>
-              Mail : info@acintyo{activeTab === "Careers" ? ".com" : ".co.in"}
-            </p>
-          </div>
-        </div>
-      </div>
       <div className="header-main">
-        <div className="top-header-show-button">
-          <button
-            className="header-changing-button"
-            onClick={handleTopHeaderChangeClick}
-          >
-            {isTopHeaderShowing ? <BiArrowToTop /> : <BiArrowFromTop />}
-          </button>
-        </div>
-        <div className="header-sub-main">
-          <img
-            src="https://acintyo.co.in/images/logo.png"
-            width={170}
-            alt="acintyo logo"
-          />
-          <ul className="header-nav-list">{content}</ul>
-          <button onClick={handleSmallNavBarClick} className="small-nav-icon">
-            <GiHamburgerMenu />
-            <ul className="small-nav-list">
-              {isSmallNavShowing && smallNavContent}
-            </ul>
-          </button>
-        </div>
-        {/* <ul className="small-nav-list">
-          {isSmallNavShowing && smallNavContent}
-        </ul> */}
-      </div>
-      <div className={`page-heading-section ${isHome && "is-home"}`}>
-        <div className="page-heading-sub">
-          {!isHome && (
-            <h1 className="page-main-heading">{pageHeading[0].navItem}</h1>
-          )}
-        </div>
+        <img
+          src="https://acintyo.co.in/images/logo.png"
+          width={200}
+          alt="acintyo logo"
+        />
+        <ul className="header-nav-list">{content}</ul>
       </div>
     </div>
   );
